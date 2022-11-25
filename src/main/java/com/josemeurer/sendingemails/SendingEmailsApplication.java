89c1a2abbd13a06cbf6dec441df43549ae668fe7
josemeurer <josewmeurer@gmail.com>
2022-11-25 20:19:46 -0300

    first commit

---
+++ src/main/java/com/josemeurer/sendingemails/SendingEmailsApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.sendingemails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendingEmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendingEmailsApplication.class, args);
+	}
+
+}
