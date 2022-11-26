0bb4c778000c6cddea12713b5c9543f7199017fe
josemeurer <josewmeurer@gmail.com>
2022-11-26 10:34:58 -0300

    FEAT EmailService

--- src/main/java/com/josemeurer/sendingemails/controllers/EmailController.java
+++ src/main/java/com/josemeurer/sendingemails/controllers/EmailController.java
@@ -1,7 +1,12 @@
 package com.josemeurer.sendingemails.controllers;
 
 import com.josemeurer.sendingemails.dto.EmailDTO;
+import com.josemeurer.sendingemails.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/emails")
 public class EmailController {
 
+    @Autowired
+    private EmailService emailService;
+
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        emailService.sendEmail(dto);
         return ResponseEntity.noContent().build();
     }
 }
